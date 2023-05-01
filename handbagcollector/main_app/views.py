b58795413564ed62d0d98f76bb471a4461bb0d3f
Sally-Kam <skam2000@gmail.com>
2023-05-01 17:04:22 -0400

    simulate handbag data

--- handbagcollector/main_app/views.py
+++ handbagcollector/main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+handbags = [
+  {'name': 'Rogue 25 in Colorblock with Tea Rose', 'price': 895, 'description': 'Glovetanned leather. One credit card slot. Inside snap and multifunction pockets. Zip closure, fabric and leather lining. Two open compartments', 'brand': 'Coach', 'color': 'Brass/Bubblegum Multi'},
+  {'name': 'LOULOU SMALL CHAIN BAG IN QUILTED "Y" LEATHER', 'price': 3450, 'description': 'BAG WITH FRONT FLAP AND MAGNETIC SNAP CLOSURE DECORATED WITH THE CASSANDRE AND Y-QUILTED OVERSTITCHING.', 'brand': 'Saint Laurent', 'color': 'Greyish Brown'},
+]
+# Define the home view
+def home(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'home.html')
