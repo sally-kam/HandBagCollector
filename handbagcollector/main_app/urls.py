adc59d35cd6c7669e1b2a5e21ae272eb5826f7b5
Sally-Kam <skam2000@gmail.com>
2023-05-01 17:23:25 -0400

    add handbags index with all the details of each handbag

--- handbagcollector/main_app/urls.py
+++ handbagcollector/main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    # route for handbags index
+    path('handbags/', views.handbags_index, name='index'),
 ]
--- handbagcollector/main_app/views.py
+++ handbagcollector/main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 handbags = [
-  {'name': 'Rogue 25 in Colorblock with Tea Rose', 'price': 895, 'description': 'Glovetanned leather. One credit card slot. Inside snap and multifunction pockets. Zip closure, fabric and leather lining. Two open compartments', 'brand': 'Coach', 'color': 'Brass/Bubblegum Multi'},
-  {'name': 'LOULOU SMALL CHAIN BAG IN QUILTED "Y" LEATHER', 'price': 3450, 'description': 'BAG WITH FRONT FLAP AND MAGNETIC SNAP CLOSURE DECORATED WITH THE CASSANDRE AND Y-QUILTED OVERSTITCHING.', 'brand': 'Saint Laurent', 'color': 'Greyish Brown'},
+  {'name': 'Rogue 25 in Colorblock with Tea Rose', 'price': 895, 'description': 'Glovetanned leather. One credit card slot. Inside snap and multifunction pockets. Zip closure, fabric and leather lining. Two open compartments.', 'brand': 'Coach', 'color': 'Brass/Bubblegum Multi'},
+  {'name': 'LOULOU SMALL CHAIN BAG IN QUILTED "Y" LEATHER', 'price': 3450, 'description': 'Bag with font flap and magnetic snap closure. Decorated with the cassandre and Y-quilted overstitching.', 'brand': 'Saint Laurent', 'color': 'Greyish Brown'},
 ]
 # Define the home view
 def home(request):
@@ -11,3 +11,8 @@ def home(request):
 def about(request):
   # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'about.html')
+
+def handbags_index(request):
+  return render(request, 'handbags/index.html', {
+    'handbags': handbags
+  })
