42ad2f620e43190f813ef3194470dd88802111c8
Sally-Kam <skam2000@gmail.com>
2023-05-02 15:17:56 -0400

    create handbag model

--- handbagcollector/main_app/models.py
+++ handbagcollector/main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Handbag(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField(max_length=500)
+    brand = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    image = models.CharField(max_length=200)
