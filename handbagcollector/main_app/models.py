5221029738612d454670542835a574394644432e
Sally-Kam <skam2000@gmail.com>
2023-05-03 18:17:43 -0400

    add redirect in the models.py

--- handbagcollector/main_app/models.py
+++ handbagcollector/main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Handbag(models.Model):
@@ -11,3 +12,7 @@ class Handbag(models.Model):
 
     def __str__(self):
         return self.name
+    
+      # Add this method
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'cat_id': self.id})
