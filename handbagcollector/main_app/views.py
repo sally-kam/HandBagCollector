152cecf38e5d3401f672e73948a152a5c6dbea11
Sally-Kam <skam2000@gmail.com>
2023-05-03 18:27:36 -0400

    subclass update view and deleteview, then customize form template

--- handbagcollector/main_app/views.py
+++ handbagcollector/main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Handbag
 
 # Define the home view
@@ -24,3 +24,11 @@ def handbags_detail(request, handbag_id):
 class HandbagCreate(CreateView):
   model = Handbag
   fields = '__all__'
+
+class HandbagUpdate(UpdateView):
+  model = Handbag
+  fields = ['description', 'brand', 'color', 'image']
+
+class HandbagDelete(DeleteView):
+  model = Handbag
+  success_url = '/handbags'
