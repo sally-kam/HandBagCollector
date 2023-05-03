111e90e5d73fef44b458e2baff9354f86cc944e4
Sally-Kam <skam2000@gmail.com>
2023-05-03 19:03:24 -0400

    add new model to the admin portal

--- handbagcollector/main_app/admin.py
+++ handbagcollector/main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Handbag
+from .models import Handbag, Worn
 
 # Register your models here.
 admin.site.register(Handbag)
+admin.site.register(Worn)
